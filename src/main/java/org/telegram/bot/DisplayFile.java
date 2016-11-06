fbed1bbf5d2de8b46a226c86379da81e21fd6fc0
liketechnik <flowa2000@gmail.com>
2016-11-06 22:58:31 +0100

    Corrected mistake in copyright statement.

--- src/main/java/org/telegram/bot/DisplayFile.java
+++ src/main/java/org/telegram/bot/DisplayFile.java
@@ -14,7 +14,7 @@
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program.  If not, see <https://github.com/rubenlagus/TelegramBots>.
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  *
  * infoDisplay uses TelegramBots Java API <https://github.com/rubenlagus/TelegramBots> by Ruben Bermudez.
  * TelegramBots API is licensed under GNU General Public License version 3 <https://github.com/rubenlagus/TelegramBots>.
