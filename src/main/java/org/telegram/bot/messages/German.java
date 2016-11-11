8fb3778ce88f8f90d22560b8662896af641e9769
Florian Warzecha <flowa2000@gmail.com>
2016-11-11 15:11:15 +0100

    Added Message class which will return the message in the language set by a user or on english and german if the user did not specify a language yet. Added German and English class that will contain the messages. Added the message strings for the startCommand and a example method for getting the startMessage.

---
+++ src/main/java/org/telegram/bot/messages/German.java
@@ -0,0 +1,45 @@
+/*
+ * Copyright (C) 2016  Florian Warzecha <flowa2000@gmail.com>
+ *
+ * This file is part of infoDisplay.
+ *
+ * infoDisplay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * infoDisplay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * infoDisplay uses TelegramBots Java API <https://github.com/rubenlagus/TelegramBots> by Ruben Bermudez.
+ * TelegramBots API is licensed under GNU General Public License version 3 <https://www.gnu.org/licenses/gpl-3.0.de.html>.
+ *
+ * infoDisplay uses parts of the Apache Commons project <https://commons.apache.org/>.
+ * Apache commons is licensed under the Apache License Version 2.0 <http://www.apache.org/licenses/>.
+ *
+ * infoDisplay uses vlcj library <http://capricasoftware.co.uk/#/projects/vlcj>.
+ * vlcj is licensed under GNU General Public License version 3 <https://www.gnu.org/licenses/gpl-3.0.de.html>.
+ *
+ * Thanks to all the people who contributed to the projects that make this
+ * program possible.
+ */
+
+package org.telegram.bot.messages;
+
+/**
+ * @author florian
+ * @version 1.0
+ * @date 10 of November of 2016
+ */
+public class German {
+    public static final String START_COMMAND_1 = "Hallo ";
+    public static final String START_COMMAND_2 = ",\nwenn du diesen Bot kennst, weißt du wahrscheinlich schon wie er " +
+            "zu bedienen ist, ansonsten benutze den '/help' Befehl. Wenn du diesen Bot noch nicht kennst, wirst " +
+            "wahrscheinlich nichts mit ihm Anfangen können. Wenn du eine Frage zu diesem Bot hast, stelle " +
+            "sie mit '/ask'.";
+}
---
+++ src/main/java/org/telegram/bot/messages/Message.java
@@ -0,0 +1,50 @@
+/*
+ * Copyright (C) 2016  Florian Warzecha <flowa2000@gmail.com>
+ *
+ * This file is part of infoDisplay.
+ *
+ * infoDisplay is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * infoDisplay is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ * infoDisplay uses TelegramBots Java API <https://github.com/rubenlagus/TelegramBots> by Ruben Bermudez.
+ * TelegramBots API is licensed under GNU General Public License version 3 <https://www.gnu.org/licenses/gpl-3.0.de.html>.
+ *
+ * infoDisplay uses parts of the Apache Commons project <https://commons.apache.org/>.
+ * Apache commons is licensed under the Apache License Version 2.0 <http://www.apache.org/licenses/>.
+ *
+ * infoDisplay uses vlcj library <http://capricasoftware.co.uk/#/projects/vlcj>.
+ * vlcj is licensed under GNU General Public License version 3 <https://www.gnu.org/licenses/gpl-3.0.de.html>.
+ *
+ * Thanks to all the people who contributed to the projects that make this
+ * program possible.
+ */
+
+package org.telegram.bot.messages;
+
+import org.telegram.telegrambots.api.objects.User;
+
+/**
+ * @author Florian Warzecha
+ * @version 1.0
+ * @date 09 of November of 2016
+ *
+ * Get messages in a users foreign language.
+ */
+public class Message {
+
+    public String getStartMessage(User user) {
+        StringBuilder startMessage = new StringBuilder();
+
+        return startMessage.toString();
+    }
+}
